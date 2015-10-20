5f4d8e1f8f15347fcab7e7d735ef20a4026c3559
Travis CI <travis@example.com>
2015-10-20 12:16:17 +0200

    Init Commit With Tests and Program

---
+++ NUnit Palindrome/Library1.fs
@@ -0,0 +1,21 @@
+﻿module NUnit_Palindrome
+
+open System
+open System.Collections
+open NUnit.Framework
+open NUnit.Framework.Constraints
+    
+[<Test>]
+let IsTrue() =
+    let isPalindrome aLista = 
+      List.rev aLista = aLista
+
+      in
+
+[<Test>]
+let That1() =
+    Assert.That(2+2=4)
+
+[<Test>]
+let That2() =
+    Assert.That(2+2=4, Is.True)
